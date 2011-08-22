73dfbdc6ba9f8cb7ff582a9878d437ad707034ef
Stpn Bltln <this@noletterword.com>
2011-08-22 15:43:16 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StarApp::Application.config.secret_token = '9daed77a28fd8e2a92ef8158f1a73678178334dfa7589499601673183ea9a1511da993ca642ad921780e55144eb0d34ea857557d53b4788c3517569972327175'
